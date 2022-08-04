f8fe1f0c8bed655a9b5498d5cd733d8da37d27f9
Dimoglo Yevgenii <222175@ecc.ac.jp>
2022-08-05 01:32:35 +0900

    Added layer separation for level

--- JoJoChan/Assets/Character2DConrtoller.cs
+++ JoJoChan/Assets/Character2DConrtoller.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Character2DConrtoller : MonoBehaviour
 {
-    public float MovementSpeed = 1;
-    public float JumpForce = 1;
+    public float MovementSpeed = 5;
+    public float JumpForce = 80;
 
     private Rigidbody2D _rigidbody;
 
@@ -16,6 +16,11 @@ public class Character2DConrtoller : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (!Mathf.Approximately(0, movement))
+        {
+            transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
