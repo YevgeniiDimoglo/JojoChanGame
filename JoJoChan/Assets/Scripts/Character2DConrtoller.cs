b154faaaef0ff56319364bad00a4c990ee2238fb
Dimoglo Yevgenii <222175@ecc.ac.jp>
2022-08-14 22:35:43 +0900

    ジョジョちゃんモーション（ジャンプ）を実施

--- JoJoChan/Assets/Scripts/Character2DConrtoller.cs
+++ JoJoChan/Assets/Scripts/Character2DConrtoller.cs
@@ -1,21 +1,78 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Character2DConrtoller : MonoBehaviour
 {
     public float MovementSpeed = 10;
     public float JumpForce = 50;
 
+    private bool m_Grounded;
+    const float k_GroundedRadius = .2f;
+
+    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform m_CeilingCheck;
+
+    [Header("Events")]
+    [Space]
+
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
     public Animator animator;
 
     private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+
+    }
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        m_Grounded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+    }
+
     private void Update()
     {
+
+        if (Input.GetButtonDown("Jump") && m_Grounded)
+        {
+            animator.SetBool("IsJumping", true);
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
@@ -24,11 +81,6 @@ public class Character2DConrtoller : MonoBehaviour
         if (!Mathf.Approximately(0, movement))
         {
             transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
-        }
-
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
-        {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
     }
 }
