921d607842369d404145caee8835994012cd31fb
Dimoglo Yevgenii <222175@ecc.ac.jp>
2022-08-22 00:46:58 +0900

    ESCでゲームを終了する。
    FIX

--- JoJoChan/Assets/Scripts/Exit.cs
+++ JoJoChan/Assets/Scripts/Exit.cs
@@ -14,7 +14,7 @@ public class Exit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
