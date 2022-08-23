4c2d0a1b47a44967736f38ea1c581d62726c4d3f
Dimoglo Yevgenii <222175@ecc.ac.jp>
2022-08-24 00:42:35 +0900

    最初の能力を使い

---
+++ JoJoChan/Assets/Scripts/ClockLiquid.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockLiquid : MonoBehaviour
+{
+    public PowerUse power;
+
+    private float ColorValue;
+
+    private SpriteRenderer _spriteRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRender = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorValue = power.elapsedTime;
+        _spriteRender.color = new Color(1.0f, 0.0f, 0.0f, 1.0f * ColorValue * 0.1f);
+    }
+}
--- JoJoChan/Assets/Scripts/PowerUse.cs
+++ JoJoChan/Assets/Scripts/PowerUse.cs
@@ -1,20 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUse : MonoBehaviour
 {
-    GameObject JojoChan;
-    GameObject JojoChanShadow;
+    public Character2DConrtoller JojoChan;
+    public Character2DConrtoller JojoChanShadow;
+
+    public float elapsedTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        JojoChan = GameObject.Find("JoJoChan");
-        JojoChanShadow = GameObject.Find("JoJoChanShadow");
-
-        JojoChan.SetActive(true);
-        JojoChanShadow.SetActive(false);
+        JojoChan.gameObject.SetActive(true);
+        JojoChanShadow.gameObject.SetActive(false);
+        elapsedTime = 10.0f;
     }
 
     // Update is called once per frame
@@ -22,18 +23,33 @@ public class PowerUse : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if (JojoChan.activeSelf == true)
+            if (JojoChan.CharacterState != 2)
             {
-                JojoChan.SetActive(false);
-                JojoChanShadow.SetActive(true);
+                JojoChan.CharacterState = 2;
+                JojoChanShadow.gameObject.SetActive(true);
                 JojoChanShadow.transform.position = JojoChan.transform.position;
             }
             else
             {
-                JojoChan.SetActive(true);
-                JojoChanShadow.SetActive(false);
+                JojoChan.gameObject.SetActive(true);
+                JojoChan.CharacterState = 1;
+                JojoChanShadow.gameObject.SetActive(false);
                 JojoChan.transform.position = JojoChanShadow.transform.position;
             }
+        }
+
+        if (JojoChan.CharacterState == 2)
+        {
+            elapsedTime -= Time.deltaTime;
+        }
+
+        if (elapsedTime <= 0)
+        {
+            JojoChan.gameObject.SetActive(true);
+            JojoChan.CharacterState = 1;
+            JojoChanShadow.gameObject.SetActive(false);
+            JojoChan.transform.position = JojoChanShadow.transform.position;
+            elapsedTime = 10.0f;
         }
     }
 }
