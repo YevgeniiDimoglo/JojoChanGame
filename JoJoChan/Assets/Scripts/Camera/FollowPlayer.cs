ff6181738ff2874c5513eb240add3763535fe3d1
MSI\2220069 <kelvinleungyl@gmail.com>
2022-09-28 10:30:27 +0900

    Update Smooth Camera: SmoothTime decrease when player speed > 15.0f

--- JoJoChan/Assets/Scripts/Camera/FollowPlayer.cs
+++ JoJoChan/Assets/Scripts/Camera/FollowPlayer.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 0.1f;
+    [SerializeField] private float smoothTime = 0.25f;
+    [SerializeField] private float x_dist = 0.5f;
+    private float smoothTime_adjustment;
+    private float x_dist_adjustment;
     private GameObject playerObj;
     private Player player;
     private Transform playerTransform;
     private Vector3 velocity = Vector3.zero;
-    private float smoothTime = 0.25f;
 
     // Start is called before the first frame update
     private void Start()
@@ -16,6 +20,8 @@ public class FollowPlayer : MonoBehaviour
         playerObj = GameObject.FindGameObjectWithTag("Player");
         player = playerObj.GetComponent<Player>();
         playerTransform = playerObj.transform;
+        smoothTime_adjustment = smoothTime;
+        x_dist_adjustment = x_dist;
     }
 
     private void Update()
@@ -31,7 +37,7 @@ public class FollowPlayer : MonoBehaviour
             {
                 camera.orthographicSize = 4.0f;
             }
-            transform.position += new Vector3(Input.GetAxis("Horizontal") * 0.05f, Input.GetAxis("Vertical") * 0.05f);
+            transform.position += new Vector3(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
         }
         else
         {
@@ -43,9 +49,30 @@ public class FollowPlayer : MonoBehaviour
             {
                 camera.orthographicSize = 3.0f;
             }
-            Vector3 targetPostion = playerTransform.position + new Vector3(0, 0, -10f);
 
-            transform.position = Vector3.SmoothDamp(transform.position, targetPostion, ref velocity, smoothTime);
+            float PlayerSpeed = player.GetComponent<Rigidbody2D>().velocity.magnitude;
+            if (PlayerSpeed > 15.0f)
+            {
+                smoothTime_adjustment *= 0.98f;
+                x_dist_adjustment *= 0.98f;
+                if (smoothTime_adjustment <= 0.01 && smoothTime_adjustment >= -0.01)
+                {
+                    smoothTime_adjustment = 0;
+                }
+                if (x_dist_adjustment <= 0.05 && x_dist_adjustment >= -0.05)
+                {
+                    x_dist_adjustment = 0;
+                }
+            }
+            else
+            {
+                smoothTime_adjustment = smoothTime;
+                x_dist_adjustment = x_dist;
+            }
+
+            Vector3 targetPostion = playerTransform.position + new Vector3(x_dist_adjustment * ((playerTransform.rotation.y < 0) ? -1.0f : 1.0f), 0, -10f); ;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPostion, ref velocity, smoothTime_adjustment);
         }
     }
 }
